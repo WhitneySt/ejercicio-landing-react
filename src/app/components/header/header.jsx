b5e8140c014c772406249729610f54e9fd67e4a5
WhitneySt <whitneystena418@gmail.com>
2023-07-06 10:54:28 -0500

    Agregando estilos con sass

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import NavigationBar from "../navigationBar/navigationBar.jsx";
 import Banner from "../banner/banner.jsx";
+import './header.scss';
 
 const Header = () => {
     return (
-        <>
+        <header className="header">
             <NavigationBar />
-            <Banner/>
-            
-        </>
+            <Banner/>            
+        </header>
     )
 }
 
--- src/app/components/home/home.jsx
+++ src/app/components/home/home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Header from "../header/header.jsx";
 import Main from "../main/main.jsx";
+import './home.scss';
 
 //Crear un componente
 const Home = () => {
   return (
-    <>
+    <div>
       <Header />
       <Main />
-    </>
+    </div>
   );
 };
 
